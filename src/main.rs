ab274dc65c0cbf20151b2d9bf78ca6de36420bb4
kayoosh2009 <kayoosh2009@gmail.com>
2026-07-10 16:49:24 +0300

    0.0.7

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
-use axum::{routing::get, Router};
+use axum::{routing::{get, post}, Router};
 use std::net::SocketAddr;
 use dotenv::dotenv;
 
 mod database;
+mod auth;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-    
     tracing_subscriber::fmt::init();
     
     let pool = database::init().await;
+    let state = auth::AppState { db: pool };
     
     let app = Router::new()
-        .route("/", get(|| async { "Hello" }));
+        .route("/", get(|| async { "Hello" }))
+        .route("/auth/google", post(auth::google_login))
+        .route("/auth/email", post(auth::email_login))
+        .with_state(state);
     
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::info!("Server on {}", addr);
